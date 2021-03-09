c9a340c6b0fdaa351209cd2618867afea70a88ad
davids-tips <dpowellfacebook@gmail.com>
2021-03-09 16:08:20 +0000

    test

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-
+bot = commands.Bot(command_prefix="$")
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -8,6 +8,10 @@ class Moderation(commands.Cog):
     @commands.command()
     async def foo(self, ctx):
         await ctx.send("Foo!")
+        
+@bot.command(name='testing', description='test command')
+async def name(ctx):
+    await ctx.send('hello there')
         
 def setup(bot):
     bot.add_cog(Moderation(bot))
