99803eba0c3b8a691bf04823738fdb5c9d60a59d
david powell <dpowellfacebook@gmail.com>
2021-03-13 12:19:10 -0500

    Updated 1o1room command and added it to the slash commands.

--- alpha/main.py
+++ alpha/main.py
@@ -19,13 +19,14 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 # end of logging
 print('logging setup complete')
-Token = ('Nzk4MzMxNjgxMDI2NDc0MDA0.X_zeUQ.pGkvY-bhPjUpsokDdMrwpsE_PBI')
+Token = ('Nzk4MzMxNjgxMDI2NDc0MDA0.X_zeUQ.b39yujyOOHi_ihwowi1u6h3SBPs')
 
-#creating python file to the cards against furries script
+# creating python file to the cards against furries script
 
 client = discord.Client()
 PrefixHelp = 'My Prefix is $\n'
 'Please use that with commands Thanks. - Whiskey the Developer'
+
 
 @client.event
 async def on_connect():
@@ -33,42 +34,45 @@ async def on_connect():
           'Please Wait...\n'
           )
 
+
 @client.event
 async def on_ready():
     print('Hello Master *nuzles*\n'
           'logged in and ready to accept commands\n\n'
           'Logged in as [{0.user}]\n'.format(client))
-    game = discord.Game("🐱‍💻Working With My Master🐱‍💻")
+    game = discord.Game("I am the floofy foxo🦊🦊")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
-      return
-    if message.content.startswith("OwO"):
-        await message.channel.send("OwO Whats This?")
-        if message.channel.nsfw:
-            await message.channel.send("*Notices Buldge*")
+        return
+    print("debug: msg recieved")
+    if message.content.startswith("OwO") or message.content.startswith("owo"):
+        await message.channel.send("$cmd1")
+        print('Owo ran')
 
     if message.content.startswith("nuz"):
         await message.channel.send("***nuzzles***")
-        await message.channel.send("https://cdn.discordapp.com/attachments/798736137317253170/811095654008487946/DerpysFavs_58.jpeg")
+        await message.channel.send(
+            "https://cdn.discordapp.com/attachments/798736137317253170/811095654008487946/DerpysFavs_58.jpeg")
 
     if message.content.startswith("$slave"):
         await message.channel.send("!clear 20")
     if message.content.startswith("$delete"):
         await message.delete()
 
-        #below is code for sending animated emojis
+        # below is code for sending animated emojis
     if message.content.startswith(":screamtoad:"):
-      await message.delete()
-      await message.channel.send("<a:scream:802746690993913856>")
+        await message.delete()
+        await message.channel.send("<a:scream:802746690993913856>")
         # remove the "a" from the line above for a non animated emoji and set the id to the correct id
     if message.content.startswith("dndgroup"):
-      await message.channel.send("<:thomas:802907659153834024><:fred:802907477486075934><:noah:802912601947766814><:david:802914481000677376><:aidan:802929427746914324><:gage:805583021100105728>")
+        await message.channel.send(
+            "<:thomas:802907659153834024><:fred:802907477486075934><:noah:802912601947766814><:david:802914481000677376><:aidan:802929427746914324><:gage:805583021100105728>")
     if message.content.startswith('$online?'):
-      await message.channel.send('Online! Hello Master!')
+        await message.channel.send('Online! Hello Master!')
 
     if message.content.startswith('$Hello'):
         await message.channel.send('Hello')
@@ -83,8 +87,6 @@ async def on_message(message):
         await message.channel.send(PrefixHelp)
     elif message.content.startswith('$Test'):
         await message.channel.send('Bot Online!')
-    elif message.content.startswith('$ping'):
-        await ctx.send(F'Pong! {client.latency}')
     elif message.content.startswith('$Boop'):
 
         boop = [
@@ -96,14 +98,5 @@ async def on_message(message):
     elif message.content.startswith('$Evening'):
         await message.channel.send('***Good Evening, Master!***')
 
-      
 
-
-
-
-            
-
-        
-
-
-client.run(Token) 
+client.run(Token)
--- beta/wb.py
+++ beta/wb.py
@@ -3,40 +3,59 @@
 import discord
 from discord.ext import commands
 import datetime
-import traceback
+
 bot = commands.Bot(command_prefix="$")
 ct = datetime.datetime.now()
 guild_ids = []
+
 
 @bot.event
 async def on_ready(autopost=True, case_insensitive=True):
     print('Ready!')
 
-try:
-    bot.load_extension("cogs.mod")  # Instead of a file-like or path-like string, you put `directory.file`, without a file extension.
-except:
-    print("Failed to load moderation:")
-traceback.print_exc()
+
+# try:
+#    bot.load_extension("cogs.mod")  # Instead of a file-like or path-like string, you put `directory.file`, without a file extension.
+# except:
+#    print("Failed to load moderation:")
+# traceback.print_exc()
 
 
+# command archived for future work on it
+# @bot.command(name='cmd1', description='remote owo command from other bot')
+# async def name(ctx):
+#    print('recieved')
+#    await ctx.message.delete()
+#    await ctx.send('test message')
+# end
 @bot.command(name='1o1', description='creates 1 on 1 room')
 @commands.has_permissions(manage_roles=True)
-async def name(ctx, arg, user1: discord.Member, user2: discord.Member):
-    await ctx.guild.create_role(arg, reason='Creating 1o1 room for {user1} and {user2}')
+async def name(ctx, arg, user1: discord.Member, user2: discord.Member, channel_name):
+    role = await ctx.guild.create_role(name=arg)
     message2 = []
     await ctx.send(arg)
-    print("arg=", arg)
-    rolename = arg
-    print("rolename var= ", rolename)
-    # the argument rolenamearg is from my command
-    role = discord.utils.get(ctx.guild.roles, name=arg)
+    print("arg= ", arg)
+   # role = discord.utils.get(ctx.guild.roles, name=arg)
     await user1.add_roles(role)
-    print("added role:[", rolename, '] to user1')
-    message2.append("added role:[", rolename, '] to user1')
+    print("added role:[", arg, '] to user1')
     await user2.add_roles(role)
-    print("added role:[", rolename, '] to user2')
-    message2.append("added role:[", rolename, '] to user2')
-    await ctx.send(message2)
+    print("added role:[", arg, '] to user2')
+    await ctx.send('added roles sucessfully')
+
+    overwrites = {
+        ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
+        role: discord.PermissionOverwrite(read_messages=True)
+    }
+    channel = await ctx.guild.create_text_channel(channel_name, overwrites=overwrites, nsfw=True, catergory='PRIVITE')
 
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+#avoid scrolling down to prevent token leak
+
+
+
+
+
+
+
 bot.run("Nzk4MjAxODM4ODY3NTc4OTQw.X_xlZA.7KPYmPbWKWOIpccbUY9ikaJs4B8")
